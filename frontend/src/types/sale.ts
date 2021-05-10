821c133f1e30cc438617c4fc8be78d4ab3d363b2
Vinicius Soares <vinicius.gomes.dev@gmail.com>
2021-05-10 01:52:40 -0300

    Datatable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,38 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+}
+
+export type SalePage = {
+  content?: Sale[];
+  // pageable: {
+  //   sort: {
+  //       sorted: boolean,
+  //       unsorted: boolean,
+  //       empty: boolean
+  //   },
+  //   offset: number,
+  //   pageNumber: number,
+  //   pageSize: number,
+  //   paged: boolean,
+  //   unpaged: boolean
+  // },
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  number: number;
+  size?: number,
+  first: boolean,
+  numberOfElements?: number,
+  empty?: boolean
+}
+
 export type SaleSum = {
   sellerName: string;
   sum: number;
