4b9fbfe04950ed6252f23af4fdb7b4548e76f5d4
Vinicius Soares <vinicius.gomes.dev@gmail.com>
2021-05-10 23:56:36 -0300

    Test

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+// export const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+export const BASE_URL = 'https://sds3-vinicius-soares.herokuapp.com';
